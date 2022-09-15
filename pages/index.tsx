ffbcd232d852e43fb593e311050b39a2e996bf24
manami-sato <manamisato2001@gmail.com>
2022-09-15 14:07:20 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
       <OriginalSpacer size="16px" />
       <Box
         as="img"
-        src={imgPath + 'bnr_02.png'}
+        src={imgPath + 'bnr_01.png'}
         w="100%"
         // h="240px"
         bg="lightGray"
